115cda6908b2c7db7126b509e9f0c9bb23b85a61
Jason Parent <jason.parent@carneylabs.com>
2015-02-24 16:36:43 -0500

    Added 'Cosmo' Bootstrap theme.

--- friends/settings.py
+++ friends/settings.py
@@ -90,6 +90,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'friends', 'static'),
+)
+
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'friends', 'templates'),
 )
