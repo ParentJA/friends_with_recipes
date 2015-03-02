0b1c9c41880a8fd1ba62ff4947a5149475abcb91
Jason Parent <jason.parent@carneylabs.com>
2015-03-02 13:39:07 -0500

    Added navigation bar links. Added tab bar to users home. Functional tests for basic tab bar functionality pass.

--- functional_tests/test_users.py
+++ functional_tests/test_users.py
@@ -17,7 +17,84 @@ class UsersTest(FunctionalTest):
         # John goes to the users page.
         self.browser.get(self.live_server_url + '/users/')
 
-        # He notices that the page title says 'Users'.
-        title = self.browser.find_element_by_tag_name('title')
+        # He notices that the navigation bar has a 'users' link that is active.
+        link = self.browser.find_element_by_css_selector('li.active')
+        link_a = link.find_element_by_tag_name('a')
 
-        self.assertIn('Users', title.text)
+        self.assertEqual('users-button', link_a.get_attribute('id'))
+
+        # He also notices another navigation bar that has three tabs: search, requests, and browse.
+        search_tab = self.browser.find_element_by_id('search_tab')
+
+        self.assertEqual(search_tab.text, 'Search')
+
+        requests_tab = self.browser.find_element_by_id('requests_tab')
+
+        self.assertEqual(requests_tab.text, 'Requests')
+
+        browse_tab = self.browser.find_element_by_id('browse_tab')
+
+        self.assertEqual(browse_tab.text, 'Browse')
+
+    def test_search(self):
+        # John is a logged-in user.
+        self.create_pre_authenticated_session(
+            first_name='John',
+            last_name='Carney',
+            email='john.carney@carneylabs.com',
+            password='password1'
+        )
+
+        # John goes to the users page.
+        self.browser.get(self.live_server_url + '/users/')
+
+        # He clicks the search button.
+        search_tab = self.browser.find_element_by_id('search_tab')
+        search_tab.click()
+
+        # The search pane is revealed.
+        search_pane = self.browser.find_element_by_id('search_pane')
+
+        self.assertTrue(search_pane.is_displayed())
+
+    def test_requests(self):
+        # John is a logged-in user.
+        self.create_pre_authenticated_session(
+            first_name='John',
+            last_name='Carney',
+            email='john.carney@carneylabs.com',
+            password='password1'
+        )
+
+        # John goes to the users page.
+        self.browser.get(self.live_server_url + '/users/')
+
+        # He clicks the requests button.
+        requests_tab = self.browser.find_element_by_id('requests_tab')
+        requests_tab.click()
+
+        # The requests pane is revealed.
+        requests_pane = self.browser.find_element_by_id('requests_pane')
+
+        self.assertTrue(requests_pane.is_displayed())
+
+    def test_browse(self):
+        # John is a logged-in user.
+        self.create_pre_authenticated_session(
+            first_name='John',
+            last_name='Carney',
+            email='john.carney@carneylabs.com',
+            password='password1'
+        )
+
+        # John goes to the users page.
+        self.browser.get(self.live_server_url + '/users/')
+
+        # He clicks the browse button.
+        browse_tab = self.browser.find_element_by_id('browse_tab')
+        browse_tab.click()
+
+        # The browse pane is revealed.
+        browse_pane = self.browser.find_element_by_id('browse_pane')
+
+        self.assertTrue(browse_pane.is_displayed())
